1aec53936e4168456c8d526a1718faf113522667
Ashfaque Hossain Abir <104485722+Abirett@users.noreply.github.com>
2023-06-23 22:16:29 +0545

    Updated

--- HO_functions_&_arrays.js
+++ HO_functions_&_arrays.js
@@ -44,6 +44,7 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // ____________| filter |_________________
 // --> Get 21 and Older
 
+// let canDrink = [];
 // for(let i = 0; i < ages.length; i++) {
 //     if(ages[i] >= 21) {
 //         canDrink.push(ages[i]);
